6cfa8b5a1ee7953a65bceb13ced0c0bf51ae5890
maui1030 <yellow.1030.maui@gmail.com>
2019-08-13 08:45:56 -0300

    [docs] Fixed function call to same as import. (#2477)

--- packages/app/ios/RNFBApp/RNFBVersion.h
+++ packages/app/ios/RNFBApp/RNFBVersion.h
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2016-present Invertase Limited & Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,6 @@
  *
  */
 
-export default {
-  LOADED: 'rewarded_loaded',
-  EARNED_REWARD: 'rewarded_earned_reward',
-};
+#import <Foundation/Foundation.h>
+
+extern const NSString * RNFBVersionString;
--- packages/app/scripts/genversion-ios.js
+++ packages/app/scripts/genversion-ios.js
@@ -1,4 +1,9 @@
-/*
+const fs = require('fs');
+const path = require('path');
+
+const version = require('../lib/version');
+const outputPath = path.resolve(__dirname, '..', 'ios', 'RNFBApp', 'RNFBVersion.m');
+const template = `/**
  * Copyright (c) 2016-present Invertase Limited & Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,19 +20,11 @@
  *
  */
 
-import { Platform } from 'react-native';
+#import "RCTVersion.h"
 
-export default {
-  ...Platform.select({
-    android: {
-      BANNER: 'ca-app-pub-3940256099942544/6300978111',
-      INTERSTITIAL: 'ca-app-pub-3940256099942544/1033173712',
-      REWARDED: 'ca-app-pub-3940256099942544/5224354917',
-    },
-    ios: {
-      BANNER: 'ca-app-pub-3940256099942544/2934735716',
-      INTERSTITIAL: 'ca-app-pub-3940256099942544/4411468910',
-      REWARDED: 'ca-app-pub-3940256099942544/1712485313',
-    },
-  }),
-};
+// generated file - do not modify or commit
+NSString* const RNFBVersionString = @"VERSION";
+
+`;
+
+fs.writeFileSync(outputPath, template.replace('@"VERSION"', `@"${version}"`), 'utf8');
